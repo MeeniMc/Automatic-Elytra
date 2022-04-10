0d475560b39e9cfa9d86b4834052db4ce6791261
Froxcey <danichen204@gmail.com>
2022-04-10 18:07:23 +0800

    Disable loading of main class (it's not used) and add comment to warn about this.

--- src/main/java/com/loucaskreger/automaticelytra/AutomaticElytra.java
+++ src/main/java/com/loucaskreger/automaticelytra/AutomaticElytra.java
@@ -2,6 +2,9 @@ package com.loucaskreger.automaticelytra;
 
 import net.fabricmc.api.ModInitializer;
 
+// This file is currently not loaded because it's not used
+// Change fabric.mod.json to enable it again.
+
 public class AutomaticElytra implements ModInitializer {
 
     @Override
